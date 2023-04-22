6cef3b2244e545293c130527f41e10150c49f1e5
Jared Gallina <jdgallina.github@gmail.com>
2023-04-22 14:45:10 -0600

    Starting on Tasks API functions

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,8 @@ Details of setup can be found in the Readme of the repository
 """
 
 import streamlit as st
-import oyaml, os
-from src.scheduler_lib.gcal_functions import add_event_to_calendar
+import oyaml, os, datetime
+from src.scheduler_lib.gcal_functions import get_main_task_list, get_a_task
 
 # load in the resource data
 with open(os.path.abspath('src/scheduler_resources/default_events.yaml'), 'r') as rid:
@@ -18,7 +18,78 @@ st.sidebar.title("Calendar Event(s) Details")
 
 # provide the user the default schedules to use
 general_choice = st.sidebar.radio('Select a Base Option or Customize from Scratch',
-                                  [*[event['name'] for event in resource_data['events']], "Custom"])
+                                  [*[event for event in resource_data['events'].keys()], "Custom"])
 st.sidebar.header(f"Current Configuration for {general_choice}")
+
+# handle the details for a given experiment
+time_points = st.sidebar.number_input("Number of Time Events:", min_value=1, max_value=10, value=len(resource_data['events'][general_choice]['time_events']), step=1)
+with st.sidebar.form("Experiment Details"):
+
+    # dict to store all user inputs
+    user_inputs_results = {'time_events': [{} for i in range(time_points)]}
+
+    # display the user input options
+    st.header("User Inputs")
+    for user_input in resource_data['events'][general_choice]['user_inputs']:
+
+        # number inputs
+        if user_input['type'] == 'number':
+            user_inputs_results[user_input['name']] = st.number_input(user_input['name'], min_value=user_input['min'],
+                                                                      max_value=user_input['max'], step=user_input['step'])
+
+        # selectbox inputs
+        elif user_input['type'] == 'single_select':
+            user_inputs_results[user_input['name']] = st.selectbox(user_input['name'], options=user_input['options'])
+
+        # string inputs
+        elif user_input['type'] == 'string':
+            user_inputs_results[user_input['name']] = st.text_input(user_input['name'])
+
+        # date inputs
+        elif user_input['type'] == 'date':
+            user_inputs_results[user_input['name']] = st.date_input(user_input['name'] + " (YYYY/MM/DD)", value=datetime.date.today())
+
+    # display the time events
+    st.header("Time Events")
+    for ind in range(time_points):
+        event_name = resource_data['events'][general_choice]['time_events'][ind]['title'] if ind < len(resource_data['events'][general_choice]['time_events']) else "Event Name"
+        event_days = resource_data['events'][general_choice]['time_events'][ind]['days'] if ind < len(resource_data['events'][general_choice]['time_events']) else 30
+
+        user_inputs_results['time_events'][ind]['title'] = st.text_input(f"Name of Event {ind}", value=event_name)
+        user_inputs_results['time_events'][ind]['day_offset'] = st.number_input(f"Days to Event {ind}", value=event_days,
+                                                                           min_value=0, max_value=360, step=1)
+
+    # submit
+    submit_spec = st.form_submit_button("Verify My Choices")
+    if submit_spec:
+        with open('_tmp_spec.yaml', 'w') as wid:
+            oyaml.dump(user_inputs_results, wid)
+
+# display the generated details in the main panel
+# for user final review before submitting
+if os.path.isfile('_tmp_spec.yaml'):
+    with open('_tmp_spec.yaml', 'r') as rid:
+        user_genned_data = oyaml.safe_load(rid)
+        st.text_area("User Created Specification", str(user_genned_data))
+else:
+    user_genned_data = None
+
+# provide final submit button
+if user_genned_data is not None:
+    final_submit = st.button("Submit to Google Calendar!")
+
+    if final_submit:
+        st.write("Submitted!")
+
+        # call the submit method
+        task_lists = get_a_task()
+        st.write("Results")
+        st.write(task_lists)
+
+        # now remove the temp file
+        os.remove('_tmp_spec.yaml')
+
+
+
 
 
--- src/scheduler_lib/gcal_functions.py
+++ src/scheduler_lib/gcal_functions.py
@@ -1,7 +1,6 @@
-"""
-This file implements the required functionality for interacting with
-Google calendar
-"""
+from __future__ import print_function
+
+import os.path
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,5 +8,98 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-def add_event_to_calendar():
-    pass
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/tasks.readonly']
+
+path_to_file = os.path.abspath(__file__)
+resource_dir = os.path.join(os.path.dirname(os.path.dirname(path_to_file)), 'scheduler_resources')
+
+
+def _update_token():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(f'{resource_dir}/token.json'):
+        creds = Credentials.from_authorized_user_file(f'{resource_dir}/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                f'{resource_dir}/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(f'{resource_dir}/token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+def get_10_task_lists():
+    """Shows basic usage of the Tasks API.
+    Prints the title and ID of the first 10 task lists.
+    """
+    creds = _update_token()
+    try:
+        service = build('tasks', 'v1', credentials=creds)
+
+        # Call the Tasks API
+        results = service.tasklists().list(maxResults=10).execute()
+        items = results.get('items', [])
+
+        if not items:
+            return ['No task lists found.']
+
+        return [(item['title'], item['id']) for item in items]
+
+    except HttpError as err:
+        return err
+
+def get_main_task_list():
+    """Shows basic usage of the Tasks API.
+    Prints the title and ID of the first 10 task lists.
+    """
+    creds = _update_token()
+    try:
+        service = build('tasks', 'v1', credentials=creds)
+
+        # Call the Tasks API
+        results = service.tasklists().list(maxResults=1).execute()
+        items = results.get('items', [])
+
+        if not items:
+            print("No task lists found")
+            return None
+
+        return items[0]['title'], items[0]['id']
+
+    except HttpError as err:
+        print(err)
+        return None
+
+def get_a_task():
+    creds = _update_token()
+
+    tasklist_details = get_main_task_list()
+    if tasklist_details is None:
+        print("Error finding the tasklist")
+
+    else:
+        tasklist_id = tasklist_details[0]
+
+        try:
+            service = build('tasks', 'v1', credentials=creds)
+
+            results = service.tasks().list(tasklist=tasklist_id, maxResults=1).execute()
+            items = results.get('items', [])
+
+            if not items:
+                print("No task lists found")
+                return None
+
+            return items[0]['title'], items[0]['id']
+
+        except HttpError as err:
+            print(err)
+            return None
